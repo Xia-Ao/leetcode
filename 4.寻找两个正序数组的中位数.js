554b5db62b31457c2289df448e8b723e682a2568
xiaao <xiaao@youzan.com>
2021-01-15 20:59:47 +0800

    feat: 4 11 未完成

---
+++ 4.寻找两个正序数组的中位数.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=javascript
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  // 1. 暴力方法
+  const all = [...nums1, ...nums2].sort((a, b) => a - b);
+  let mid = all.length >> 1;
+  const isOdd = Boolean(all.length % 2);
+  const result = isOdd ? all[mid] : (all[mid - 1] + all[mid]) / 2;
+  console.log(all);
+  console.log(result);
+  return result
+
+  // 方法二
+};
+
+// findMedianSortedArrays([1, 2], [3, 4]);
+// findMedianSortedArrays([3], [-2, -1]);
+// @lc code=end
+
