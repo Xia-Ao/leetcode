9c5f95cb7d5e2a6d01f31f6b6175b8dec646fae4
xiaao <xiaao@guang.com>
2021-01-26 23:52:56 +0800

    feat: 18 init

---
+++ 18.四数之和.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=javascript
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+  // 同三数之和一样 排序 + 双指针
+};
+// @lc code=end
+
