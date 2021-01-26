ebf2cfa1d753232b5f9ceee1ced9a68244bfb57f
xiaao <xiaao@guang.com>
2021-01-26 23:49:20 +0800

    feat: 16

--- 16.最接近的三数之和.js
+++ 16.最接近的三数之和.js
@@ -11,51 +11,41 @@
  * @return {number}
  */
 var threeSumClosest = function (nums, target) {
-
   // 排序 + 双指针
   nums = nums.sort((a, b) => a - b);
-  console.log(nums);
-
   // 与target最小的差距
   let minDiff = Number.MAX_SAFE_INTEGER;
   let res = null;
-  let lastDiff = null;
-
   // 第一层遍历
-  for (let i = 0; i < nums.length; i++) {
-
+  for (let i = 0; i < nums.length - 2; i++) {
     // 双指针
     let L = i + 1;
     let R = nums.length - 1;
     let target2 = target - nums[i];
-    // if (target2 > minDiff) continue;
 
     while (L < R) {
       console.log(i, L, R);
       let rest = nums[L] + nums[R];
       // 如果存在比上一个最小差距更小的值， 则更新
       let diff = target2 - rest;
-      if (Math.abs(diff) < minDiff) {
-        minDiff = Math.abs(diff);
-        res = rest + nums[i];
-      }
-      console.log('--', diff, lastDiff, rest);
-      if (diff >= 0 && ) {
+      if (diff >= 0) {
         L++
       } else {
         R--;
       }
-      lastDiff = diff;
+      let newDiff = Math.abs(diff);
+      if (newDiff < minDiff) {
+        minDiff = newDiff;
+        res = rest + nums[i];
+      }
 
     }
-    // console.log(i, L, R);
   }
 
-  console.log(res);
   return res;
 };
 
 // threeSumClosest([0,2,1,-3], 1);
-threeSumClosest([1, 2, 4, 8, 16, 32, 64, 128], 82);
+// threeSumClosest([1, 2, 4, 8, 16, 32, 64, 128], 82);
 // @lc code=end
 
