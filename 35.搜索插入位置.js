cc66ad61da92799df901a1cfc4ce3f84b91ce06b
xiaao <xiaao@guang.com>
2021-02-02 23:42:42 +0800

    feat: 35

--- 35.搜索插入位置.js
+++ 35.搜索插入位置.js
@@ -10,7 +10,22 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
+var searchInsert = function (nums, target) {
+  // 二分法
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = (left + right) >> 1;
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    }
+  }
+  return left ;
 
 };
 // @lc code=end
