5465e372b97146dbf838a8a2e8553365433db80c
xiaao <xiaao@youzan.com>
2021-02-19 21:32:13 +0800

    feat: 41

--- 41.缺失的第一个正数.js
+++ 41.缺失的第一个正数.js
@@ -10,34 +10,66 @@
  * @return {number}
  */
 var firstMissingPositive = function (nums) {
-  // 哈希表
+  /**
+   * 方法一： 哈希表
+   */
+  // const N = nums.length;
+  // let res = N + 1;
+
+  // // 1. 将<=0 的数变成N+1
+  // for (let i = 0; i < N; i++) {
+  //   if (nums[i] <= 0) {
+  //     nums[i] = N + 1;
+  //   }
+  // }
+
+  // // 2. 将 0 < |x| <= N 的数，对应的 |x|- 1 下标 位置的数打上标
+  // for (let i = 0; i < N; i++) {
+  //   const a = Math.abs(nums[i])
+  //   if (0 < a && a <= N && nums[a - 1] > 0) {
+  //     nums[a - 1] = -nums[a - 1];
+  //   }
+  // }
+
+  // // 3 返回第一个非负的下标 + 1 ，如果没有则返回N+1，数组长度+1
+  // for (let i = 0; i < N; i++) {
+  //   if (nums[i] > 0) {
+  //     res = i + 1;
+  //     break;
+  //   }
+  // }
+  // return res;
+
+  /**
+   * 方法二： 置换
+   */
   const N = nums.length;
   let res = N + 1;
 
-  // 1. 将<=0 的数变成N+1
+  // 1. 按照下标置换
   for (let i = 0; i < N; i++) {
-    if (nums[i] <= 0) {
-      nums[i] = N + 1;
+    let x = nums[i];
+    if (0 < x && x <= N && nums[x - 1] !== x) {
+      let temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+      // nums[i] = nums[x - 1];
+      // nums[x - 1] = x;
+      console.log('nums', nums);
     }
   }
-
-  // 2. 将 0 < |x| <= N 的数，对应的 |x|- 1 下标 位置的数打上标
+  // 2. 返回最小的正整数的下标+1
+  console.log(nums);
   for (let i = 0; i < N; i++) {
-    const a = Math.abs(nums[i])
-    if (0 < a && a <= N && nums[a - 1] > 0) {
-      nums[a - 1] = -nums[a - 1];
+    if (nums[i] !== i + 1) {
+      console.log('---', i)
+      return i + 1;
     }
   }
-
-  // 3 返回第一个非负的下标 + 1 ，如果没有则返回N+1，数组长度+1
-  for (let i = 0; i < N; i++) {
-    if (nums[i] > 0) {
-      res = i + 1;
-      break;
-    }
-  }
-
   return res;
+
 };
+
+console.log(firstMissingPositive([3, 4, -1, 1]));
 // @lc code=end
 
