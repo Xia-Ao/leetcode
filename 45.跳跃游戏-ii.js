e5c3e87857c084729f9f987c501fcb1edfb427b2
xiaao <xiaao@youzan.com>
2021-06-01 19:10:37 +0800

    feat: 45

--- 45.跳跃游戏-ii.js
+++ 45.跳跃游戏-ii.js
@@ -6,11 +6,25 @@
 
 // @lc code=start
 /**
- * 
+ * 贪心算法：从左往右查找
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function(nums) {
+var jump = function (nums) {
+  let len = nums.length;
+  let maxPosition = 0;
+  let steps = 0;
+  let end = 0
+
+  // 边界情况，不需要访问最后一个位置，不然会增加一次跳跃
+  for (let i = 0; i < len - 1; i++) {
+    maxPosition = Math.max(maxPosition, i + nums[i])
+    if (i === end) {
+      end = maxPosition;
+      steps++;
+    }
+  }
+  return steps;
 
 };
 // @lc code=end
