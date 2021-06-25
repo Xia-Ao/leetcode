726db56d34bdfc9ded150772d62a9c250a4bfffe
xiaao <xiaao@youzan.com>
2021-06-25 10:25:37 +0800

    feat: 56

--- 56.合并区间.js
+++ 56.合并区间.js
@@ -10,17 +10,36 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-  let 
 
   /**
-   * 左端排序: 列表按照左端数据排序，当左边的所有数据都比某一个右边左端数据小时，可以合并为一个区间
+   * 左端排序: 列表按照左端数据排序，
+   * 创建一个merge数组，先取第一个
+   * 用merge数字依次和其他数据对比，当被比较数据左端值小于merge数组的最大值，merge数组的右端值取取两个右端的最大值，
+   * 一直到merge数组的右端数据小于被比较的左端数据，此时merge为一个合并区间，开始下一个merge遍历
    */
   intervals = intervals.sort((a, b) => a[0] - b[0]);
-  console.log(intervals);
 
+  let res = [];
+
+  let merge = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    // merge右端值小于比较的左端，输出一个区间，采用新的merge
+    if (merge[1] < intervals[i][0]) {
+      res.push(merge);
+      merge = intervals[i];
+    } else {
+      // 否则合并最大值
+      merge[1] = Math.max(merge[1], intervals[i][1]);
+    }
+  }
+  // 将最后一个也push进来
+  res.push(merge);
+
+  return res;
 };
 
 
 // @lc code=end
 
-merge([[1,3],[2,6],[8,10],[15,18]]);
+merge([[1, 3], [2, 6], [8, 10], [15, 18]]);
