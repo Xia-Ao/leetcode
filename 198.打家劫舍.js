4fbec7c798664df063f7c205b7ab447b0813bd2d
xiaao <xiaao@youzan.com>
2021-08-24 10:22:34 +0800

    feat: 198

--- 198.打家劫舍.js
+++ 198.打家劫舍.js
@@ -15,14 +15,26 @@ var rob = function (nums) {
 
   const n = nums.length;
   if (n === 1) return nums[0]; // 边界， 只有一间房子
-  let dp = new Array(0).fill(0);
-  dp[0] = nums[0];
-  dp[1] = Math.max(nums[0], nums[1]);
+
+  // 固定数组 
+  // let dp = new Array(0).fill(0);
+  // dp[0] = nums[0];
+  // dp[1] = Math.max(nums[0], nums[1]);
+  // for (let i = 2; i < n; i++) {
+  //   dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  // }
+  // return dp[n - 1];
+
+  // 滚动数组
+  let first = nums[0];
+  let second = Math.max(nums[0], nums[1]);
 
   for (let i = 2; i < n; i++) {
-    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    let temp = second;
+    second = Math.max(first + nums[i], second);
+    first = temp;
   }
-  return dp[n - 1];
+  return second;
 
 };
 // @lc code=end
