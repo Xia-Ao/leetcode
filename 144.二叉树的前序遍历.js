4f405e20dfebe2a1cce4ba1526a06991af737642
xiaao <xiaao@guang.com>
2021-08-30 00:32:29 +0800

    feat: 94 144 145

---
+++ 144.二叉树的前序遍历.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  const res = [];
+  function inorder(root) {
+    if (!root) {
+      return
+    }
+    res.push(root.val);
+    inorder(root.left);
+    inorder(root.right);
+  }
+  inorder(root);
+  return res;
+};
+// @lc code=end
+
---
+++ 145.二叉树的后序遍历.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+  const res = [];
+  function inorder(root) {
+    if (!root) {
+      return
+    } 
+    inorder(root.left);
+    inorder(root.right);
+    res.push(root.val);
+  }
+  inorder(root);
+  return res;
+};
+// @lc code=end
+
