2d6f9c6715446d212fb309c767745ae607dcd5ac
xiaao <xiaao@guang.com>
2022-02-25 21:28:29 +0800

    feat: 86

---
+++ 143.重排链表.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=javascript
+ *
+ * [143] 重排链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+
+};
+// @lc code=end
+
--- 86.分隔链表.js
+++ 86.分隔链表.js
@@ -19,54 +19,28 @@
  */
 var partition = function (head, x) {
   /**
-   * 思路：找到第一个x，从x和前一个位置切断链表，再从x往后遍历，
-   * 如果val小于x，将val位置拿出，遍历前置链表，放到合适的位置，再将后置链表 前后位置接上
-   * 最后将切断的链表接上
+   * 思路：两个辅助链表，small，big ，small放小的，big放大的，最后收尾拼接起来即可
    */
 
-  if (!head || !head.next) return head;
-  // let newHead = new ListNode(-1);
-  // 1. 找到第一个x
-  let beforeNode = new ListNode(null, head);
-  while (beforeNode && beforeNode.next && beforeNode.next.val !== x) {
-    beforeNode = beforeNode.next;
-  }
-  // console.log(beforeNode.val);
-  // 2. 切断链表
-  let newHead = beforeNode.next;
-  let current = newHead;
-  beforeNode.next = null;
-  // console.log(beforeNode, head);
+  let small = new ListNode(-1);
+  let big = new ListNode(-1);
 
-  // 3. 从x往后遍历
-  while (current && current.next) {
-    if (current.next.val < x) {
-      // 当前val接到后面
-      insert(current.next.val, head)
-      // 删除当前val
-      current.next = current.next.next;
-      current = current.next;
+  let current = head;
+  let smallCurrent = small;
+  let bigCurrent = big;
+  while (current) {
+    if (current.val < x) {
+      smallCurrent.next = new ListNode(current.val);
+      smallCurrent = smallCurrent.next;
+    } else {
+      bigCurrent.next = new ListNode(current.val);
+      bigCurrent = bigCurrent.next;
     }
+    current = current.next;
   }
+  smallCurrent.next = big.next;
+  return small.next;
 
-  function insert(val, head) {
-    let current = new ListNode(null, head);
-    while (current && current.next) {
-      // 找到位置
-      if (current.next.val > val) {
-        // 插入
-        let newNode = new ListNode(val, current.next);
-        current.next = newNode;
-        return;
-      }
-    }
-
-  }
-
-  // 4. 将切断的链表接上
-  beforeNode.next = newHead;
-
-  return head;
 };
 // @lc code=end
 
