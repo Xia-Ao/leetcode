23d3484a22bf9fdf11cf91594c97cb2991546e8e
xiaao <xiaao@guang.com>
2022-07-22 21:08:33 +0800

    feat: 283

---
+++ 283.移动零.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  // 移动0 ：移动的次数偏多
+  // 向前移动非0数组
+  let len = nums.length;
+  for (let i = 0; i < len; i++) {
+    if (nums[i] !== 0) {
+      let j = i;
+      while (nums[j] !== 0) {
+        nums
+      }
+    }
+  }
+
+};
+// @lc code=end
+
