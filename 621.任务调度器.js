ebe04fb97d5f2a50c6e9c6369a67caaa9e490732
xiaao <xiaao@guang.com>
2023-03-07 23:29:44 +0800

    feat: 621

--- 621.任务调度器.js
+++ 621.任务调度器.js
@@ -22,42 +22,40 @@ var leastInterval = function (tasks, n) {
       map.set(key, 1);
     }
   }
-  // // 按照单任务数量 从大到小排序
-  let taskNums = [...map.values()].sort((a, b) => b - a);
-  console.log('taskNums', taskNums);
+  tasks = [...map.values()];
 
-  let count = 0;
-  while (taskNums.length) {
-    let i = 0;
-    console.log('循环', taskNums)
+  let step = 0;
+  while (tasks.length) {
+    // 每次循环时 任务数量需要重新排序
+    tasks.sort((a, b) => b - a);
+    // console.log('循环', tasks)
+    const len = tasks.length;
+    let k = 0; // step
+    let i = 0; // 数组下标
+
     // 步进 间隔n 实际需要走n+1步
-    let len = taskNums.length;
-    while (i <= n && len) {
-      // if (!taskNums[i]) {
-
-      //   break;
-      // };
-      // 如果没有任务，则表示间隔不够，需要待命 n-i 时间 进入下一次
-      if (i === len) {
-        console.log('待命i', i, n - i);
-        count += n - i + 1;
+    while (k <= n && tasks.length) {
+      // 如果走完数组所有任务，k还比n小，需要待命 n-k+1 时间 进入下一次循环
+      if (k === len) {
+        step += n - k + 1;
         break;
       }
-      taskNums[i]--;
-      count++;
+      tasks[i]--; // 任务数-1
+      step++; 
       // 该任务被执行完了，从taskNums 将该任务移除
-      if (taskNums[i] === 0) {
-        taskNums.splice(i, 1);
+      if (tasks[i] === 0) {
+        tasks.splice(i, 1);
+        i--;
       }
       i++;
-      // console.log('finaly i', i);
-
+      k++;
     }
   }
-  console.log(count);
-  return count;
+  // console.log(step);
+  return step;
 
 };
 // @lc code=end
 
-leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2);
+// leastInterval(["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"], 2);
+// leastInterval(["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E"], 2);
