b0f7f531a6a93a2c2aae241d246738708d367e06
xiaao <xiaao@youzan.com>
2024-02-05 21:36:23 +0800

    feat: tree

--- 337.打家劫舍-iii.js
+++ 337.打家劫舍-iii.js
@@ -19,127 +19,22 @@
  */
 var rob = function (root) {
 
+  function postOrderDfs(root) {
+    if (!root) return [0, 0];
+
+    const right = postOrderDfs(root.right)
+    const left = postOrderDfs(root.left)
+
+    // console.log('val', root.val)
+    // 抢 当前 和左右节点 不抢的值
+    const rob = root.val + right[1] + left[1]
+    // 不抢 左右子节点最大的值
+    const noRob = Math.max(...right) + Math.max(...left)
+    return [rob, noRob]
+
+  }
+
+  const res = postOrderDfs(root);
+  return Math.max(...res)
 };
 // @lc code=end
-
-
-// 实现 JSON.stringify
-function jsonStringify(obj) {
-  let type = typeof obj;
-
-  if (type !== "object" || obj === null) {
-    if (/string|undefined/.test(type)) {
-      obj = '"' + obj + '"';
-    }
-    return String(obj);
-  } else {
-    let json = [];
-    let arr = Array.isArray(obj);
-
-    for (let k in obj) {
-      let v = obj[k];
-      let type = typeof v;
-      if (/string|undefined/.test(type)) {
-        v = '"' + v + '"';
-      } else if (type === "object") {
-        v = jsonStringify(v);
-      }
-      json.push((arr ? "" : '"' + k + '":') + String(v));
-    }
-
-    return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-  }
-}
-
-// 实现 JSON.parse
-function jsonParse(jsonStr) {
-  let index = 0;
-  let currentChar = jsonStr[0];
-
-  function next() {
-    index++;
-    currentChar = jsonStr[index];
-  }
-
-  function parseValue() {
-    if (currentChar === '"') {
-      return parseString();
-    } else if (currentChar === '{') {
-      return parseObject();
-    } else if (currentChar === '[') {
-      return parseArray();
-    } else {
-      return parseNumber();
-    }
-  }
-
-  function parseString() {
-    let str = '';
-    while (jsonStr[++index] !== '"') {
-      str += jsonStr[index];
-    }
-    next();
-    return str;
-  }
-
-  function parseObject() {
-    let obj = {};
-    next();
-    while (currentChar !== '}') {
-      let key = parseString();
-      next();
-      let value = parseValue();
-      obj[key] = value;
-      if (currentChar === ',') {
-        next();
-      }
-    }
-    next();
-    return obj;
-  }
-
-  function parseArray() {
-    let arr = [];
-    next();
-    while (currentChar !== ']') {
-      arr.push(parseValue());
-      if (currentChar === ',') {
-        next();
-      }
-    }
-    next();
-    return arr;
-  }
-
-  function parseNumber() {
-    let numStr = '';
-    while (!isNaN(currentChar)) {
-      numStr += currentChar;
-      next();
-    }
-    return Number(numStr);
-  }
-
-  return parseValue();
-}
-
-const aa = {
-  a: 1,
-  b: 'str',
-  c: [1, 23],
-  d: {
-    da: 1,
-    db: 'str',
-    dc: [1, 23],
-    dd: {
-      da: 1,
-      db: 'str',
-      dc: [1, 23],
-      dd: {}
-    }
-  }
-}
-
-const str = jsonStringify(aa);
-console.log(str);
-console.log(jsonParse(str))
---
+++ 508.出现次数最多的子树元素和.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=508 lang=javascript
+ *
+ * [508] 出现次数最多的子树元素和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findFrequentTreeSum = function (root) {
+  const map = {};
+
+  function dfs(root) {
+    if (!root) return null;
+    let res
+    if (!root.left && !root.right) {
+      res = root.val;
+      map[res] = map[res] === undefined ? 1 : map[res]++;
+      return res;
+    };
+    const left = dfs(root.left);
+    const right = dfs(root.right);
+    res = left + right + root.val;
+    // console.log('res', res, map, map[res] === undefined)
+    map[res] = map[res] === undefined ? 1 : map[res] + 1;
+    return res;
+  }
+  dfs(root);
+  console.log(map);
+
+
+  let max = 0;
+  let res = []
+  for (let key in map) {
+    // console.log('max', max, map[key] === max)
+    if (map[key] === max) {
+      res.push(key);
+    } else if (map[key] > max) {
+      max = map[key];
+      res = [key]
+    }
+  }
+  return res;
+
+};
+// @lc code=end
+
