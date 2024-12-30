e0d8ea8374a4542aa0d1753d6ab35148b0f5fd3e
xiaao <xiaao@guang.com>
2024-12-30 23:14:53 +0800

    feat: tree

---
+++ 107.二叉树的层序遍历-ii.ts
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=typescript
+ *
+ * [107] 二叉树的层序遍历 II
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Medium (70.77%)
+ * Likes:    541
+ * Dislikes: 0
+ * Total Accepted:    193.2K
+ * Total Submissions: 273K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[15,7],[9,20],[3]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[[1]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  function bfs(node: TreeNode | null, depth: number, res: number[][]) {
+    if (!node) return res;
+    if (res[depth] === undefined) res[depth] = [];
+    res[depth].push(node.val);
+    bfs(node.left, depth + 1, res);
+    bfs(node.right, depth + 1, res);
+    return res;
+  }
+  return bfs(root, 0, []).reverse();
+}
+// @lc code=end
